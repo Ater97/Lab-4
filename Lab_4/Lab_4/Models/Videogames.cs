7ba2ece04919b00fccfa0ecb99f4ea758c72464e
Sebastián Orantes Chang <sebastian.orantes.chang@gmail.com>
2017-04-27 07:26:09 -0600

    First blood

---
+++ Lab_4/Lab_4/Models/Videogames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab_4.Models
+{
+    public class Videogames
+    {
+
+        public string Name { get; set; }
+
+        public DateTime RealeseDAte { get; set; }
+        public string Gender { get; set; }
+        public int Ranking { get; set; }
+        public string platform { get; set; }
+
+    }
+}
